57ecd59022c17901de445e4c35f1a6641bf4776c
AGFO <aguiar.f@hotmail.fr>
2013-11-22 12:15:10 +0100

    Version 1.0

---
+++ GSB JAVA/src/Controleur/Main.java
@@ -0,0 +1,12 @@
+package Controleur;
+
+import Vue.V_accueil;
+
+public class Main {
+
+	public static void main(String[] args) {
+		@SuppressWarnings("unused")
+		V_accueil start = new V_accueil();
+	}
+
+}
